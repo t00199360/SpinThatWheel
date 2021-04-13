5d6ccc52be05d93ea5653f1a564c12fac22f8391
t00199360 <t00199360@gmail.com>
2021-04-13 18:38:47 +0100

    tested functionality of unity ads
    + resolved conflicts between package manager advertisements and unity store advertisements
    + Ran test function for unity ads

--- Assets/Scripts/UnityAds.cs
+++ Assets/Scripts/UnityAds.cs
@@ -1,18 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Advertisements;
 
 public class UnityAds : MonoBehaviour
 {
+    string GooglePlayID = "4087783";
+    bool TestMode = true;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Advertisement.Initialize(GooglePlayID, TestMode);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void displayInterstitialAd()
     {
-        
+        Advertisement.Show();
     }
 }
